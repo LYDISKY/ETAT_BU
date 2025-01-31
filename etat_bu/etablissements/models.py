141b2886524edbc7ded3dc3c640256863d605416
Lydie MIDJRATO <lydmid05@gmail.com>
2025-01-31 08:27:57 +0000

    modification du modele etablissements

--- etat_bu/etablissements/models.py
+++ etat_bu/etablissements/models.py
@@ -18,19 +18,8 @@ class Etablissement(models.Model):
     contact = models.CharField(max_length=20)
     logo = models.ImageField(upload_to='logos/', blank = True, null= True)
     type_etablissement = models.CharField(max_length=10, choices=TYPE_CHOICES, default='ecole')
-    decoupage_annuel = models.CharField(
-        max_length=10, 
-        choices=DECOUPAGE_CHOICES, 
-        default='trimestre'
-    )
+    decoupage_annuel = models.CharField(max_length=10, choices=DECOUPAGE_CHOICES, default='trimestre')
 
-    def save(self, *args, **kwargs):
-        """ Assigner automatiquement le découpage en fonction du type d'établissement """
-        if self.type_etablissement == 'universite':
-            self.decoupage_annuel = 'semestre'
-        else:
-            self.decoupage_annuel = 'trimestre'
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.nom
